b2b91843a854185af39682abbaf2759f7711f1ab
Elliot Thyrant <elliot.thyrant2001@gmail.com>
2022-01-20 18:31:58 +0100

    rendu 05/03/02

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: 5, category: "french"}
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: 4, category: "french"}
+yopizz =  { name: "Yopizz", address: "Loin tres loi", phone_number: 4, category: "french"}
+kushy =  { name: "Kushy", address: "599 street", phone_number: 5, category: "french"}
+augo =  { name: "Augo", address: "5632 line", phone_number: 4, category: "french"}
+
+
+[ dishoom, pizza_east, yopizz, kushy, augo].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
+
+#
+#puts "Creating Reviews..."
+#r1 = {5, "sdfbgnh,jklkljh"}
+#r2 = {4, "dfgbvds"}
+#r3 = {5, "dfghjk,;l:mkjhgfdsfghjgfdfgvbvd"}
+#
+#
+#[r1, r2, r3].each do |attributes|
+#  review = Review.create!(attributes)
+#  puts "Created rev"
+#end
+#puts "Finished!"
+#
